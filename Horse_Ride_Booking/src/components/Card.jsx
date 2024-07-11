543feb4577a759c7abd96d2ed759c810df799a3c
Udit parmar <udittparmar@gmail.com>
2024-07-11 21:59:01 +0530

    working in booking conformation

--- Horse_Ride_Booking/src/components/Card.jsx
+++ Horse_Ride_Booking/src/components/Card.jsx
@@ -1,93 +1,60 @@
-import PropTypes from 'prop-types';
-import styles from './Card.module.css';
+import PropTypes from "prop-types";
+import styles from "./Card.module.css";
+import HorseRideBookingForm from "./HorseRideBookingForm";
+import { useState } from "react";
 
-const Card = ({ horseData,handleBookRide }) => {
-  // const handleBookRide=(horseId)=>{
-  //   console.log(horseId)
-    
+const Card = ({ horseData }) => {
+  const [showModal, setshowModal] = useState(false);
+  const [selectedHorse, setselectedHorse] = useState({ id: "", price: 0 });
 
-  // }
-
-  console.log(horseData);
+  const handleBookRide = (id, price) => {
+    setselectedHorse({ id, price });
+    setshowModal(true);
+  };
+  const closeModal = () => {
+    setshowModal(false);
+    setselectedHorse({ id: "", price: 0 });
+  };
 
   return (
     <>
-    
-    <div className={`${styles.parentCard}`}>
-    <p className="text-left">Available Rides</p>
-    
-      {Array.isArray(horseData) && horseData.map((horse) => (
-        
-          <div className={`card ${styles.cardItems}`} key={horse.horseId} style={{ width: '18rem' }}>
-            <img className="card-img-top" src={horse.img} alt="Card image cap" />
-            <div className="card-body">
-              <h4 className="card-title">{horse.horseName}</h4>
+      <div className={`${styles.parentCard}`}>
+        <p className="text-left">Available Rides</p>
 
-              {/* Button trigger modal */}
-              <button type="button" className="btn btn-primary" data-toggle="modal" onClick={() => handleBookRide(horse.id)} data-target="#exampleModalLong">
-                Book now {horse.price}
-              </button>
+        {Array.isArray(horseData) &&
+          horseData.map((horse) => (
+            <div
+              className={`card ${styles.cardItems}`}
+              key={horse.horseId}
+              style={{ width: "18rem" }}
+            >
+              <img
+                className="card-img-top"
+                src={horse.img}
+                alt="Card image cap"
+              />
+              <div className="card-body">
+                <h4 className="card-title">{horse.horseName}</h4>
 
-              {/* Modal
-              <div className="modal fade" id="exampleModalLong" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalLongTitle">Book horse rideing</h5>
-                      <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div className="modal-body">
-                      <form>
-                        <div className="form-group">
-                          {horseData.map((horse) => (
-                            <div key={horse.value}>
-                              <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <input
-                                  type="checkbox"
-                                  className="form-check-input"
-                                  id={horse.value}
-                                  value={horse.value}
-                                />
-                                <img className="card-img-top" src={horse.img} alt="Card image cap" style={{height:'110px',width:'240px'}}/>
-                                <label className="form-check-label" htmlFor={horse.horseName} style={{padding:'10px'}}>
-                                  {horse.horseName}
-                                </label>
-                              </div>
-                              <hr />
-                            </div>
-                          ))}
-                        </div>
-                        <div className="form-group">
-                          <label htmlFor="name">Name</label>
-                          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
-                        </div>
-
-                        <div className="form-group">
-                          <label htmlFor="number">Phone no.</label>
-                          <input type="number" className="form-control" id="number" placeholder="Enter your phone number" />
-                        </div>
-                        <div className="form-group">
-                          <label htmlFor="Email">Email</label>
-                          <input type="email" className="form-control" id="Email" placeholder="Enter email" />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                      </form>
-                    </div>
-                    <div className="modal-footer">
-                      <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                      <button type="button" className="btn btn-primary">Save changes</button>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
-            </div>
-          </div>
-
-      ))}
+                {/* Button trigger modal */}
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-toggle="modal"
+                  onClick={() => handleBookRide(horse.id, horse.price)}
+                >
+                  Book now {horse.price}
+                </button>
               </div>
-              {/* <HorseRideBookingForm handleBookRide={handleBookRide} horseData={horseData}/> */}
+            </div>
+          ))}
+      </div>
+      <HorseRideBookingForm
+        show={showModal}
+        onclose={closeModal}
+        horseId={selectedHorse.id}
+        horsePrice={selectedHorse.price}
+      />
     </>
   );
 };
--- Horse_Ride_Booking/src/components/Home.jsx
+++ Horse_Ride_Booking/src/components/Home.jsx
@@ -1,44 +1,43 @@
-import Carousel from './Carousel';
-import Navbar from './Navbar';
-import Card from './Card';
-import { useState } from 'react';
+import Carousel from "./Carousel";
+import Navbar from "./Navbar";
+import Card from "./Card";
+import { useState } from "react";
 
-import HorseRideBookingForm from'./HorseRideBookingForm';
-const Home = (handleBookRide) => {
-    const [horseData,setHorseData]=useState([{
-        img :'src/assets/horse_crousel.jpg',
-        horseId:1,
-        horseName:"white young horse",
-        price:5000
+const Home = () => {
+  const [horseData, setHorseData] = useState([
+    {
+      img: "src/assets/white_horse.jpg",
+      horseId: 1,
+      horseName: "white young horse",
+      price: 5000,
+    },
+    {
+      img: "src/assets/rasing_horse.png",
+      horseId: 2,
+      horseName: "raceing horse",
+      price: 20000,
+    },
+    {
+      img: "src/assets/horse_crousel.jpg",
+      horseId: 3,
+      horseName: "black young horse",
+      price: 10000,
+    },
+    {
+      img: "src/assets/Marwari_horse.jpg",
+      horseId: 4,
+      horseName: "MarWari_horse",
+      price: 15000,
+    },
+  ]);
 
-    },{
-        img :'src/assets/horse_crousel.jpg',
-        horseId:2,
-        horseName:"raceing ",
-        price:20000
-        
-    },{
-        img :'src/assets/horse_crousel.jpg',
-        horseId:3,
-        horseName:"black young horse",
-        price:10000
-        
-    },{
-        img :'src/assets/horse_crousel.jpg',
-        horseId:4,
-        horseName:"white young",
-        price:15000
-        
-    }])
-
-    return(
-        <>
-        <Navbar/>
-        <Carousel/>
-        <Card horseData={horseData} handleBookRide={handleBookRide}/>
-        <HorseRideBookingForm handleBookRide={handleBookRide}/>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+      <Card horseData={horseData} />
+    </>
+  );
+};
 
 export default Home;
